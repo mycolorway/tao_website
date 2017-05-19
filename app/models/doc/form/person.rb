a3c35db5c9f6149360dfa51e0a500b2d81d16def
farthinker <farthinker@gmail.com>
2017-05-20 01:35:43 +0800

    ADD: group select demo

--- app/models/doc/form/person.rb
+++ app/models/doc/form/person.rb
@@ -4,7 +4,8 @@ module Doc
     class Person
       include ActiveModel::Model
 
-      attr_accessor :name, :nickname, :email, :age, :gender, :comment, :married, :handsome, :colors
+      attr_accessor :name, :nickname, :email, :age, :gender, :comment,
+        :married, :handsome, :colors, :city
 
       validates_presence_of :name, :email
 
