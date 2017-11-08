96eab9d130512bbdc0f55e41f933a6f7294244ae
farthinker <farthinker@gmail.com>
2017-11-09 00:07:35 +0800

    CHG: update deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -88,8 +88,8 @@ task :deploy do
     invoke :'bundle:install'
 #    invoke :'rails:db_migrate'
     # invoke :'rails:assets_precompile'
-    command %{yarn}
-    command %{NODE_ENV=production RAILS_ENV=production bundle exec rails webpacker:compile}
+    queue %{yarn install}
+    queue %{NODE_ENV=production RAILS_ENV=production bundle exec rails webpacker:compile}
     invoke :'deploy:cleanup'
 
     to :launch do
